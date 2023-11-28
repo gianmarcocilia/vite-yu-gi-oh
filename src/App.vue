b0a6b927f8357c88a1facd63af4eb95466593976
gianmarcocilia <gianmarcocilia97@gmail.com>
2023-11-28 14:12:49 +0100

    add: filter none for archetype select

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script>
 import axios from 'axios';
-import {store} from './store.js'
+import { store } from './store.js'
 import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
 import Select from './components/Select.vue';
@@ -18,18 +18,27 @@ export default {
             this.store.loading = false;
         })
     },
-    components: {AppHeader, AppMain, Select},
+    components: { AppHeader, AppMain, Select },
     methods: {
         onChange() {
+            console.log(this.store.searchText);
             this.store.loading = true;
-        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0', {
-            params: {
-                archetype: this.store.searchText,
-            },
-        }).then((resp) => {
-            this.store.cards = resp.data.data;
-            this.store.loading = false;
-        })
+            if (this.store.searchText) {
+                axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0', {
+                    params: {
+                        archetype: this.store.searchText,
+                    },
+                }).then((resp) => {
+                    this.store.cards = resp.data.data;
+                    this.store.loading = false;
+                })
+            } else {
+                this.store.loading = true;
+                axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then((resp) => {
+                    this.store.cards = resp.data.data;
+                    this.store.loading = false;
+                })
+            }
         }
     }
 }
@@ -38,12 +47,10 @@ export default {
 
 <template>
     <AppHeader />
-    <Select value1="Alien" value2="Ally of Justice" value3="Ancient Gear" @selectedValue="onChange()"/>
+    <Select value1="Alien" value2="Ally of Justice" value3="Ancient Gear" @selectedValue="onChange()" />
     <AppMain />
-  
 </template>
 
 <style lang="scss">
 @use './style/general.scss';
-
 </style>
--- src/components/Select.vue
+++ src/components/Select.vue
@@ -17,6 +17,7 @@ export default {
 <template>
 <div>
     <select name="mySelect" @change="$emit('selectedValue')" v-model="store.searchText">
+    <option value="">Nessun filtro</option>
     <option :value="value1">{{ value1 }}</option>
     <option :value="value2">{{value2}}</option>
     <option :value="value3">{{ value3 }}</option>
